a577aac143f906b66f8ba783b673989bbce11bf9
Ryan Liang <ryan7x7x@gmail.com>
2019-06-15 02:20:17 -0700

    added bonus feature to rotate log file and name old log files  with time-stamp

--- server/app.js
+++ server/app.js
@@ -4,8 +4,16 @@ const path = require('path');
 const csv = require('csvtojson');
 
 const folderPath = path.join(__dirname, '../logs/');
-const fileName = folderPath+'log.csv';
+const logName = 'log';
+const fileType = '.csv'
+const partialFileName = folderPath + logName;
+const fileName = folderPath + logName + fileType;
 const logFileHeader = 'Agent,Time,Method,Resource,Version,Status\n';
+const rotateLogFileLimit = 20;
+let checkCurrentLogFileLine = true;
+let currentLogFileLineNumber = 0;
+let logFileIsRenamed = false;
+
 
 const app = express();
 
@@ -14,16 +22,7 @@ if (!fs.existsSync(folderPath)){
     fs.mkdirSync(folderPath);
 }
 
-//check if log file exists
-fs.stat(fileName, function(err, stat) {
-    if(err == null) {
-        //do nothing if file exists else create log file and add header to top of the file content
-    } 
-    else if(err.code == 'ENOENT') {
-        appendContent(logFileHeader);
-    } 
-});
-
+//for logging
 app.use((req, res, next) => {
     let userAgent = req.headers["user-agent"].replace(/\, /g, ' ');
     let date = new Date().toISOString();
@@ -34,30 +33,94 @@ app.use((req, res, next) => {
     let statusCode = res.statusCode;
     let output = userAgent+','+date+','+method+','+resource+','+protocol+version+','+statusCode+'\n';
 
+    console.log(output);
     appendContent(output);
 
-    console.log(output);
     next()
 });
 
 app.get('/', (req, res) => {
-    res.json("ok");
+    res.status(200).json("ok");
 
 });
 
 app.get('/logs', (req, res) => {
-    csv()
-    .fromFile(fileName)
-    .then((result) => {
-        res.json(result);
+    //check if log file exists
+    fs.stat(fileName, function(err, stat) {
+        if(err == null) {
+            csv()
+            .fromFile(fileName)
+            .then((result) => {
+                res.status(200).json(result);
+            });
+        } 
+        else if(err.code == 'ENOENT') {
+            res.status(404).json("Log file not found");
+        } 
     });
- });
 
- function appendContent(content){
+});
+
+function appendContent(content){
+    //check if log file exists
+    fs.stat(fileName, function(err, stat) {
+        //while log file is being renamed, fs.stat still sees it as existed
+        if(err == null && logFileIsRenamed != true) {
+            if (checkCurrentLogFileLine){
+                checkCurrentLogFileLine = false;
+                let text = fs.readFileSync(fileName).toString();
+                let lines = text.split('\n');
+                currentLogFileLineNumber = lines.length - 1
+            }
+
+            appendToFile(content);
+        } 
+        else if(err.code == 'ENOENT') {
+            //if file doesn't exist, create log file and add header to top of the file content
+            logFileIsRenamed = false;
+            currentLogFileLineNumber = 0;
+            appendToFile(logFileHeader, ()=>{
+                checkCurrentLogFileLine = false;
+                appendToFile(content);
+            });
+        } 
+    });
+}
+
+function appendToFile(content, callback=null){
     //file is created automatically if not existed 
     fs.appendFile(fileName, content, function (err) {
-        if (err) 
+        if (err){ 
             throw err;
+        }
+        currentLogFileLineNumber++;
+        rotateLogFile(()=>{
+            if (callback !== null){
+                callback();
+            }
+        });
     });
- }
+}
+
+function rotateLogFile(callback = null){
+    if (currentLogFileLineNumber > rotateLogFileLimit){
+        let newFileName = partialFileName + ' ' + new Date().toISOString() + fileType;
+        logFileIsRenamed = true;
+        fs.rename(fileName, newFileName, function (err) {
+            if (err){
+                throw err;
+            }
+            currentLogFileLineNumber = 0;  
+            if (callback !== null){
+                callback();
+            }
+        });
+    }
+    else{
+        if (callback !== null){
+            callback();
+        }
+    }
+}
+
 module.exports = app;
